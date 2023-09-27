a12facfea994377a69a19c010d1b602d535e1496
Martin Svendsen <Svendsenm71@gmail.com>
2023-09-27 09:17:16 +0200

    Working Project

--- Terning Kaster.cpp
+++ Terning Kaster.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
-#include <Cstdlib >
-#include <Time.h >
+#include <Cstdlib>
+#include <Time.h>
 
 using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int siderMax = 6;
+	int Terning;
+	srand(time(0));
+
+	cout << "Hvor mange terninger vil du gerne slaa med :\n";
+	cin >> Terning;
+
+	for (int i = 0; i < Terning; i++)
+	{
+		cout << "Resultatet af kast nummmer " << i << " er : " << rand() % siderMax + 1 << " \n";
+	}
+
 }
